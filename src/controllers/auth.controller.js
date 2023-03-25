f88137f1f1fc28715ee061239a58a459aebdb19e
PedroTrudes <pedro.jtrudes@gmail.com>
2023-03-25 15:20:38 -0300

    gerador de tokens com JWT

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
-import { loginService } from '../services/auth.service.js'
+import { loginService, generateToken } from '../services/auth.service.js'
+
 
 const login = async (req, res) => {
     const {email, password} = req.body;
@@ -15,7 +16,9 @@ const login = async (req, res) => {
             return res.status(400).send({message: "Usuario ou senhas estão errados!"})
         }
 
-        res.send("Logado com sucesso!")
+        const token = generateToken(user._id)
+
+        res.send({token});
 
     }catch(err) {
         res.status(500).send(err.message)
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,3 +1,6 @@
 import User from "../models/User.js";
+import jwt  from "jsonwebtoken";
 
 export const loginService = (email) => User.findOne({ email: email }).select("+password"); //falando que eu quero o email de user.email e adicionando o password na resposta
+
+export const generateToken = (id) => jwt.sign({id: id}, process.env.SECRET_JWT, {expiresIn: 86400} )
