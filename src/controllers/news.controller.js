1c586cce62d04e72ca9aa468ecce6e0c9b656559
PedroTrudes <pedro.jtrudes@gmail.com>
2023-03-29 22:13:01 -0300

    paginação criada em getAllNews

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,37 +1,78 @@
-import {createService, findAllService} from "../services/news.service.js";
+import { createService, findAllService, countNews } from "../services/news.service.js";
 
-const createNews = async (req, res) =>{
-    try {        
-        const {title, text, banner} = req.body;
-        
-        if(!title || !text || !banner){
-            send.status(400).send({message: "Preencha os campos"})
+const createNews = async (req, res) => {
+    try {
+        const { title, text, banner } = req.body;
+        if (!title || !text || !banner) {
+            send.status(400).send({ message: "Preencha os campos" })
         }
 
         await createService({
             title,
-            text, 
+            text,
             banner,
             user: req.userId,
         });
 
-        res.status(201).send({message: "News criada com sucesso"});
+        res.status(201).send({ message: "News criada com sucesso" });
     } catch (error) {
-        res.status(500).send({message: error.message})
+        res.status(500).send({ message: error.message })
     }
 
 }
 
 const getAllNews = async (req, res) => {
     try {
-        const news = await findAllService();
-        if(news.length === 0){
-            return res.status(400).send({message: "Não nenhuma news"})
+        let { limit, offset } = req.query;
+        limit = Number(limit);
+        offset = Number(offset);
+
+        if (!limit) {
+            limit = 5;
         }
-        res.send(news)
+        if (!offset) {
+            offset = 0;
+        }
+
+        const news = await findAllService(offset, limit);
+        const total = await countNews();
+        const currentUrl = req.baseUrl;
+
+        const next = offset + limit;
+        const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+        const previous = offset - limit < 0 ? null : offset - limit;
+        const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
+
+
+        if (news.length === 0) {
+            return res.status(400).send({ message: "Não nenhuma news" })
+        }
+
+        //res.send(news)
+
+        res.send({
+            nextUrl,
+            previousUrl,
+            limit,
+            offset,
+            total,
+
+            results: news.map((item) => ({
+                id: item._id,
+                title: item.title,
+                text: item.text,
+                banner: item.banner,
+                likes: item.likes,
+                coments: item.comentes,
+                name: item.user.name,
+                username: item.user.username,
+            })),
+        });
+
     } catch (error) {
-        res.status(400).send({message: error.message})
+        res.status(400).send({ message: error.message })
     }
 }
 
-export {createNews, getAllNews}
+export { createNews, getAllNews }
